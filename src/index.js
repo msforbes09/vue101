89c98503387a5c0afcb0b564acb67abaea99c753
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-21 15:41:30 +0800

    event listener

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ window.Vue = vue;
 new Vue({
     el: '#app',
     data: {
-        message: 'Try to change this value'
+        message: 'Try to change this value',
+        tasks: ['Learn HTML5', 'Learn Css3', 'Learn Javascript',],
+        newTask: '',
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.newTask);
+            this.newTask = '';
+        }
     }
 });
